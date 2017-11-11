629e6f06a64c03ce9c082a8289aeea0a3b4168f8
ZETA\Nik <nik.green@xtra.co.nz>
2017-11-11 16:43:59 +1300

    hope this works

---
+++ Toa_mini/Intermediate/Build/Win64/Toa_mini/Development/UELinkerFixups/PCH.UELinkerFixupsName.h.cpp
@@ -0,0 +1,1 @@
+#include "D:\Users\nicholas.gre6280\Documents\GitHub\Toa_mini\Toa_mini\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
---
+++ Toa_mini/Intermediate/Build/Win64/Toa_mini/Development/UELinkerFixups/UELinkerFixupsName.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_UELINKERFIXUPSNAME_H__
+#define __AUTO_UELINKERFIXUPSNAME_H__
+//Last Write: 8/11/2017 1:08:53 a.m. +00:00
+#include "D:\Users\nicholas.gre6280\Documents\GitHub\Toa_mini\Toa_mini\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
+#endif//__AUTO_UELINKERFIXUPSNAME_H__
